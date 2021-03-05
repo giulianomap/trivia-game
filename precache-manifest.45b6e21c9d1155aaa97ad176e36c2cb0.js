34afb9a7a736761cbfa00a351aa28142c1a7b529
Giuliano Peixoto <giulianopeixoto@gmail.com>
2021-03-05 16:26:20 -0300

    Updates

---
+++ precache-manifest.45b6e21c9d1155aaa97ad176e36c2cb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76f0d7a5ab0b4583b73aeab8cf23a8d",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "86d0953f0bf36a5356af",
+    "url": "/trivia-game/static/css/main.7ee663da.chunk.css"
+  },
+  {
+    "revision": "32d0b8a321f3a4db7556",
+    "url": "/trivia-game/static/js/2.434e81d2.chunk.js"
+  },
+  {
+    "revision": "bd73a2afad2507effe472689ce2a62c0",
+    "url": "/trivia-game/static/js/2.434e81d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86d0953f0bf36a5356af",
+    "url": "/trivia-game/static/js/main.5e7b2b67.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  },
+  {
+    "revision": "2712117ae92e37bc2afe81c1dc6d2306",
+    "url": "/trivia-game/static/media/3156605.2712117a.jpg"
+  },
+  {
+    "revision": "df8665d3e226b24d8a296cb34a3692fd",
+    "url": "/trivia-game/static/media/DigitalNumbers-Regular.df8665d3.ttf"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
